9e6924d285bfe0d235b73e45092f38e23b908589
Jose A Ferrando <joseaferrando@hotmail.com>
2022-05-23 15:54:40 +0100

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Button, Card, Row } from "react-bootstrap";
+import { myService } from "./auth/msalConfig";
 
-function App() {
+export default function App() {
+
+  const getData = () => {
+   
+    const callGetWeather = () => {
+     
+      const fetchWeatherEndpoint = async () => await fetch(myService.myEndpoint);
+
+      fetchWeatherEndpoint().then(response => response.json()).then(data => console.log(data[0].date));
+      
+    };
+    
+    // get token
+
+    callGetWeather();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Card border="primary">
+      <Card.Header>Bla bla</Card.Header>
+      <Card.Body>
+        <Row>
+          <Button
+            variant="primary"
+            as="input"
+            type="button"
+            value="Get my data"
+            onClick={getData}
+          ></Button>
+        </Row>
+      </Card.Body>
+    </Card>
   );
 }
-
-export default App;
---
+++ src/auth/msalConfig.js
@@ -0,0 +1,19 @@
+export const msalConfig =
+{
+    auth: {
+        cliendId: "", 
+        authority: "",
+        redirectUri: ""
+    },
+cache: {
+    cacheLocation: ""
+}
+};
+
+export const loginRequest ={
+    scopes: []
+}
+
+export const myService = {
+    myEndpoint: "https://localhost:7135/WeatherForecast"
+}
