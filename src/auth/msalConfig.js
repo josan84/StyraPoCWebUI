f72014a5fb11aa368718f8062d02f7abe0ae89c3
Jose A Ferrando <joseaferrando@hotmail.com>
2022-06-01 10:47:08 +0100

    updating redirectUri

--- src/auth/msalConfig.js
+++ src/auth/msalConfig.js
@@ -3,7 +3,7 @@ export const msalConfig =
     auth: {
         clientId: "0ca77bae-04a1-42a1-a1e1-1d28d27d66e0", 
         authority: "https://login.microsoftonline.com/93c16d38-d1d7-4702-ab62-e9d16603afe5",
-        redirectUri: "http://localhost:3000/"
+        redirectUri: window.location.href.split("/").slice(0,3).join("/")
     },
 cache: {
     cacheLocation: "sessionStorage"
